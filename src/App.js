b1d886ffbef71c91cec5467c8774b150e614f9b2
Tara Dehdari <tara.dehdari@gmail.com>
2020-11-22 20:37:05 -0800

    added components to navbar and added navbar pic

--- src/App.js
+++ src/App.js
@@ -2,26 +2,26 @@ import React, { Component } from 'react';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
-
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="demo-big-content">
-    <Layout>
-        <Header title="Title" scroll>
+    <div style={{height: '300px', position: 'relative'}}>
+    <Layout style={{background: 'url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuISvMr87fnjGUcxM6HMdoT_EwW3S18zpbFQ&usqp=CAU) center / cover'}}>
+        <Header transparent title="Title" style={{color: 'white'}}>
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
-                <a href="/">Link</a>
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
             </Navigation>
         </Drawer>
         <Content>
