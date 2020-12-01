fc4200134b9aed79616a44c9db70fa8b2cd0d57a
Tara Dehdari <tara.dehdari@gmail.com>
2020-11-30 18:07:02 -0800

    added api project card to api tab

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 class Projects extends Component {
     constructor(props) {
@@ -10,9 +10,21 @@ class Projects extends Component {
     toggleCateories() {
         if(this.state.activeTab === 0) {
         return(
-            <div> 
-                <h1>This is APIs</h1>
-            </div>
+            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+                <CardTitle style={{color: '#fff', height:'176px', background: 'url(https://media.sproutsocial.com/uploads/2015/04/What-is-an-API.png) center / cover'}}> 
+                    API project #1 
+                </CardTitle>
+                <CardText>
+                    Lyric App used, bla bla bla bla bla. 
+                </CardText>
+                <CardActions border>
+                    <Button colored>GitHub</Button>
+                    <Button colored>LiveDemo</Button>
+                </CardActions>
+                <CardMenu style={{color: '#fff'}}>
+                    <IconButton name="share" />
+                </CardMenu>
+            </Card>
         )
     } else if(this.state.activeTab === 1) {
         return(
@@ -50,6 +62,11 @@ class Projects extends Component {
                 </Tabs>
 
                 <section className="projectgs-grid">
+                    <Grid className="projects-grid">
+                        <Cell col={12}>
+                            <div className="content">(this.toggleCateories()}</div>
+                        </Cell>
+                    </Grid>
                     {this.toggleCateories()}
                 </section>
             </div>
