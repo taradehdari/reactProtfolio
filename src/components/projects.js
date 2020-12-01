51b28c8aca196b2762bc33d05e0402b9b508c6b6
Tara Dehdari <tara.dehdari@gmail.com>
2020-11-30 17:49:24 -0800

    started styling on projects page

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,10 +1,57 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 class Projects extends Component {
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = { activeTab: 0 };
+    }
+    
+    toggleCateories() {
+        if(this.state.activeTab === 0) {
+        return(
+            <div> 
+                <h1>This is APIs</h1>
+            </div>
+        )
+    } else if(this.state.activeTab === 1) {
         return(
             <div>
-                <h1>Projects Page</h1>
+                <h1>This is Express</h1>
+            </div>
+        )        
+    } else if(this.state.activeTab === 2) {
+        return(
+            <div>
+                <h1>This is React</h1>
+            </div>
+        )
+       
+    } else if(this.state.activeTab === 3) {
+        return(
+            <div>
+                <h1>This is MongoDB</h1>
+            </div>  
+        )
+        
+    }
+
+    }
+
+    
+    render() {
+        return(
+            <div className="category-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId})} ripple>
+                    <Tab>APIs</Tab>
+                    <Tab>Express</Tab>
+                    <Tab>React</Tab>
+                    <Tab>MongoDB</Tab>
+                </Tabs>
+
+                <section className="projectgs-grid">
+                    {this.toggleCateories()}
+                </section>
             </div>
         )
     }
