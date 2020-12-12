2920ef7ffbe412dc818edc58cce4064118d99595
Tara Dehdari <tara.dehdari@gmail.com>
2020-12-12 10:23:14 -0800

    added beginning of about me page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import { Grid, Cell, List, ListItem, ListItemContent } from 'react-mdl';
 
 class About extends Component {
     render() {
         return(
-            <div>
-                <h1>About Page</h1>
+            <div className="about-body">
+            <Grid className="about-grid">
+                <Cell col={6}>
+                    <h2>beginning of about page</h2>
+                </Cell>
+            </Grid>    
             </div>
+            
         )
     }
 }
